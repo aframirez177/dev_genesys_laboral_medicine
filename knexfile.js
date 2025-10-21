c5f3621caae3f1134144c0f7fe6d6030d1751fbe
aframirez177 <af.ramirez1772@gmail.com>
2025-10-21 16:08:39 -0500

    correcicion en el knexfile para deploy

--- knexfile.js
+++ knexfile.js
@@ -23,10 +23,20 @@ const config = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: {
+      // Lee las variables individuales de server/.env
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // ¡Añade la configuración SSL!
+      ssl: { rejectUnauthorized: false }
+    },
     pool: { min: 2, max: 10 },
     migrations: {
-      directory: './server/src/database/migrations'
+      directory: './server/src/database/migrations',
+      extension: 'cjs' // <-- Asegúrate que esto también esté
     },
     seeds: {
       directory: './server/src/database/seeds'
