a8b5861c8361418d27edd9375c88a70aa9a7acd9
aframirez177 <af.ramirez1772@gmail.com>
2025-10-22 18:45:53 -0500

    fix: Corregir errores críticos en controladores 2

--- server/src/routes/matriz-riesgos.routes.js
+++ server/src/routes/matriz-riesgos.routes.js
@@ -1,7 +1,7 @@
 // server/src/routes/matriz-riesgos.routes.js
 import { Router } from 'express';
 // Importamos la nueva función del controlador
-import { handleFormSubmission } from '../controllers/matriz-riesgos.controller.js';
+/* import { handleFormSubmission } from '../controllers/matriz-riesgos.controller.js'; */
 
 const router = Router();
 
@@ -10,11 +10,11 @@ const router = Router();
  * @desc    Recibe los datos del formulario, los guarda en la BD y devuelve una URL de redirección.
  * @access  Public
  */
-router.post('/generar', async (req, res) => {
+/* router.post('/generar', async (req, res) => {
     // Simplemente llamamos a nuestro nuevo controlador.
     // Toda la lógica de BD y respuesta está ahora encapsulada allí.
     await handleFormSubmission(req, res);
-});
+}); */
 
 
 // NOTA: Más adelante añadiremos más rutas aquí, como por ejemplo:
